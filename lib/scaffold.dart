edcf1c8da849a078ce1b58864338e6c1f3537ccc
Arwan <mynameisarwan@outlook.com>
2022-08-02 09:19:02 +0700

    update

--- lib/scaffold.dart
+++ lib/scaffold.dart
@@ -6,16 +6,40 @@ class FirstScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text("This is app Bar"), actions: [
-      IconButton(
-          onPressed: () {},
+      appBar: AppBar(
+        title: const Text("This is app Bar"),
+        actions: <Widget>[
+          IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.search,
+                color: Colors.white,
+              ))
+        ],
+        leading: IconButton(
           icon: const Icon(
-            Icons.search,
+            Icons.menu,
             color: Colors.white,
-          ))
-    ]));
-    // appBar: AppBar(
-    //   title: const Text("This is Appbar"),
-    //
+          ),
+          onPressed: () {},
+        ),
+      ),
+      backgroundColor: Colors.teal.shade700,
+      body: const Center(
+        child: Text(
+          'Hello world!',
+          style: TextStyle(
+              color: Colors.white70,
+              fontWeight: FontWeight.bold,
+              fontSize: 24.0),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        hoverColor: Colors.blueGrey,
+        backgroundColor: Colors.cyan,
+        child: const Icon(Icons.add),
+      ),
+    );
   }
 }
